b5f22a2fe2b0710918bcb391e5c8a8171d254e46
Ningxin Hu <ningxin.hu@intel.com>
2021-04-25 10:44:30 +0800

    Support webnn sync API

--- src/library_html5_webnn.js
+++ src/library_html5_webnn.js
@@ -2,7 +2,7 @@ mergeInto(LibraryManager.library, {
   emscripten_webnn_create_neural_network_context__deps: ['$WebNN'],
   emscripten_webnn_create_neural_network_context__postset: 'WebNN.initManagers();',
   emscripten_webnn_create_neural_network_context: function() {
-    var context = navigator.ml.createNeuralNetworkContext();
-    return WebNN["mgrNeuralNetworkContext"].create(context);
+    var context = navigator.ml.getNeuralNetworkContext();
+    return WebNN.mgrNeuralNetworkContext.create(context);
   },
 });
